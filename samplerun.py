95f53bbd1d72aa05020305c84c5fd26fb446b01f
ljohnnes <ljohnnes@gmail.com>
2016-06-19 23:58:01 +0200

    final submittal with documentation

---
+++ samplerun.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 20:14:56 2016
+
+@author: Group F
+
+Github: https://github.com/LJohnnes/ex3groupf
+
+To install the package run:
+    pip install ex3groupf
+
+"""
+
+from  ex3groupf import FA_GroupF_Final_A1A41A6_H1H5
+"""
+With this method: FA_GroupF_Final_A1A41A6_H1H5.main()
+You can access the Main Menu to use the 5 task we develop:
+
+ 1 - A1  Automated Data Cleaning
+ 2 - A41 Automated Dummy Creation and Transformation with Automated Supervised Binning
+ 3 - A6  Automated method comparison and choosing 
+ 4 - H1   Human assisted Data Cleaning 
+ 5 - H5   Human assisted method picking 
+ 6 - Exit
+
+For the input files, the program accept the url where the file is located.
+For example when the program ask:
+    Input training filename and path (dev-sample.csv): 
+You can specify: "https://dl.dropboxusercontent.com/u/28535341/dev.csv"  as the source file
+By default it looks the file locally
+
+For the output y writes the files in the directory where the python program is running.
+"""
+FA_GroupF_Final_A1A41A6_H1H5.main()
+
+
+
+
+
